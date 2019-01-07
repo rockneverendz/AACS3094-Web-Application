d3f695c8cf51734cf2a0125eb4932a93faa4c6b0
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2019-01-07 20:42:55 +0800

    Staff Entity
    and Service formatting.

--- src/java/service/StaffService.java
+++ src/java/service/StaffService.java
@@ -2,21 +2,25 @@ package service;
 
 import entity.Staff;
 import javax.persistence.*;
-/**
- *
- * @author taita
- */
+
 public class StaffService {
+
     EntityManager em;
-    
-        public StaffService() {
+
+    public StaffService() {
         EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("AACS3094-Web-ApplicationPU");
         this.em = emfactory.createEntityManager();
         this.em.getTransaction().begin();
     }
-        
-        public Staff findStaffByID(String id) {
+
+    public Staff findStaffByID(String id) {
         Staff staff = em.find(Staff.class, id);
         return staff;
+    }
+    
+    public static void main(String[] args) {
+        StaffService ss = new StaffService();
+        Staff staff = ss.findStaffByID("tai");
+        System.out.println(staff);
     }
 }
