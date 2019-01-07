57e6ea1d56c93b7310877cf2665b7ddc77dffe69
taitai512 <taitai512@users.noreply.github.com>
2019-01-07 20:32:58 +0800

    StaffService and Staff Sign In

---
+++ src/java/service/StaffService.java
@@ -0,0 +1,22 @@
+package service;
+
+import entity.Staff;
+import javax.persistence.*;
+/**
+ *
+ * @author taita
+ */
+public class StaffService {
+    EntityManager em;
+    
+        public StaffService() {
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("AACS3094-Web-ApplicationPU");
+        this.em = emfactory.createEntityManager();
+        this.em.getTransaction().begin();
+    }
+        
+        public Staff findStaffByID(String id) {
+        Staff staff = em.find(Staff.class, id);
+        return staff;
+    }
+}
